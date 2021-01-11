0b64600900850ee16dff5240db617ae8055cfcef
LLDay <ssdenis99@gmail.com>
2021-01-11 22:46:52 +0300

    Fix converting time to string
    * Check when time is to big
    * Check when (uint64_t microseconds) has been set the highest bit because it converts into
    signed int inside boost

--- server/src/utils.cpp
+++ server/src/utils.cpp
@@ -1,5 +1,7 @@
 #include "quotes/utils.h"
+#include <bits/stdint-uintn.h>
 
+#include <boost/date_time/gregorian/greg_date.hpp>
 #include <string>
 
 #include <boost/date_time/gregorian/gregorian.hpp>
@@ -10,7 +12,14 @@ namespace quotes {
 
 std::string timeToString(uint64_t microseconds) noexcept {
     boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1));
+    boost::posix_time::ptime maxTime(boost::gregorian::date(9999, 1, 1));
     auto ptime = epoch + boost::posix_time::microseconds(microseconds);
+
+    uint64_t highestBit = 1;
+    highestBit = highestBit << 63;
+
+    if (ptime >= maxTime || microseconds & highestBit)
+        return "a time after 9999-Jan-01";
 
     return boost::posix_time::to_simple_string(ptime);
 }
